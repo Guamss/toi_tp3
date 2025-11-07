78721441041976d334eeb4c7244efbc2a7e17228
Guamss <guams@duck.com>
2025-11-07 15:09:18 +0100

    (doc): documentation de ppm.h

--- ppm.h
+++ ppm.h
@@ -1,13 +1,18 @@
 #ifndef PPM_H
 #define PPM_H
 
-
+/*
+	ppm_pixel : un pixel (r, g, b)
+*/
 struct ppm_pixel {
 	unsigned char r;
 	unsigned char g;
 	unsigned char b;
 };
 
+/*
+	ppm_image_setpixel() : définit la couleur d’un pixel
+*/
 static inline void ppm_setpixel(
 		struct ppm_pixel *px,
 		unsigned char r, unsigned char g, unsigned char b)
@@ -17,13 +22,22 @@ static inline void ppm_setpixel(
 	px->b = b;
 }
 
+/*
+	ppm_image : une image (largeur, hauteur, tableau de pixels)
+*/
 struct ppm_image {
 	unsigned int width;
 	unsigned int height;
 	struct ppm_pixel *px;
 };
 
+/*
+	ppm_image_init()     : initialise une image
+*/
 int ppm_image_init(struct ppm_image *im, int w, int h);
+/*
+	ppm_image_release()  : libère la mémoire
+*/
 int ppm_image_release(struct ppm_image *im);
 
 static inline void ppm_image_setpixel(
@@ -34,6 +48,9 @@ static inline void ppm_image_setpixel(
 	ppm_setpixel(px, r, g, b);
 }
 
+/*
+	ppm_image_dump()     : écrit l’image dans un fichier .ppm
+*/
 int ppm_image_dump(struct ppm_image *im, char *path);
 
 
